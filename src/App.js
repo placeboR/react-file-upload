17684b6398b6a04d71d049d7721728e73e297c64
placeboR <d.s_ht@hotmail.com>
2021-02-17 19:56:44 -0500

    500KB limit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Componet } from "react";
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import UploadFiles from "./components/upload-files.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container" style={{width: "600px"}}>
+        <div style={{margin: "20px"}}>
+          <h3>React Files Upload</h3>
+          <h4>with Spring Boot, Bootstrap</h4>
+        </div>
+        <UploadFiles/>
+      </div>
   );
 }
 
